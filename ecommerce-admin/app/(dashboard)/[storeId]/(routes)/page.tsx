8505272d0b9de1ac46f6b4169aa6460ec46d3d52
saiahhoussem <houcemesaiah@gmail.com>
2023-11-28 11:28:57 -0500

    Navigation bar

--- ecommerce-admin/app/(dashboard)/[storeId]/(routes)/page.tsx
+++ ecommerce-admin/app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -1,9 +1,22 @@
-const DashboardPage = () => {
-    return (
+import prismadb from "@/lib/prismadb";
+
+interface DashboardPageProps{
+    params: {storeId: string}
+}
+
+const DashboardPage: React.FC<DashboardPageProps> = async({
+    params
+}) => {
+    const store = await prismadb.store.findFirst({
+        where: {
+            id: params.storeId
+        }
+    });
+
+    return(
         <div>
-            This is a Dashboard!
+            Active Store: {store?.name}
         </div>
     );
 }
-
 export default DashboardPage;
